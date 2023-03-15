cc1c39ba53b6e339d34010fbef5c35a388c9e052
MinhPC11-SE161312 <minhpcse161312@fpt.edu.vn>
2023-03-15 21:20:31 +0700

    BE

---
+++ index.js
@@ -0,0 +1,60 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import xss from "xss-clean";
+import bodyParser from "body-parser";
+import rateLimiter from "express-rate-limit";
+import postRoutes from "./routes/post.js";
+import userRoutes from "./routes/users.js";
+const app = express();
+dotenv.config();
+
+app.set("trust proxy", 1);
+app.use(express.json({ limit: "50mb" }));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  })
+);
+app.use(helmet());
+app.use(xss());
+app.use(cors());
+
+const connect = async () => {
+  try {
+    await mongoose.connect('mongodb://127.0.0.1:27017/project', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("connected to mongodb");
+  } catch (error) {
+    console.log(error);
+  }
+};
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDB disconnected");
+});
+
+app.use("/posts", postRoutes);
+app.use("/user", userRoutes);
+
+const PORT = process.env.PORT || 5000;
+const start = async () => {
+  try {
+    await connect();
+    app.listen(PORT, () => {
+      console.log(`connected to port: ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
